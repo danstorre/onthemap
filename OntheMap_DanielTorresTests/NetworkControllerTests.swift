bff4961e290a8edb1fce9ee96e0368dded7891eb
Daniel Torres <danielt@blanclabs.com>
2016-12-17 15:32:22 -0400

    adding files to gitignore

--- OntheMap_DanielTorresTests/NetworkControllerTests.swift
+++ OntheMap_DanielTorresTests/NetworkControllerTests.swift
@@ -53,7 +53,7 @@ class NetworkControllerTests: XCTestCase {
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = "{\"udacity\": {\"username\": \"\", \"password\": \"\"}}".data(using: String.Encoding.utf8)
+        request.httpBody = "{\"udacity\": {\"username\": \"danstorre@gmail.com\", \"password\": \"cloudingBot$90\"}}".data(using: String.Encoding.utf8)
         let session = URLSession.shared
         let task = session.dataTask(with: request as URLRequest) { data, response, error in
             if error != nil { // Handle error…
@@ -61,17 +61,18 @@ class NetworkControllerTests: XCTestCase {
             }
             let range = Range(uncheckedBounds: (5, data!.count - 5))
             let newData = data?.subdata(in: range) /* subset response data! */
-//            networkController.convertDataWithCompletionHandler(data!, completionHandlerForConvertData: { (dataParsed,error) in
+//            networkController.convertDataWithCompletionHandler(newData!, completionHandlerForConvertData: { (dataParsed,error) in
 //            
 //                guard error == nil else {
 //                    return print("the error code: \(error!.code)")
 //                }
 //                
 //                if let dataParsed = dataParsed {
-//                    print(NSString(data: dataParsed as! Data, encoding: String.Encoding.utf8.rawValue)!)
+//                    //print(NSString(data: dataParsed as! Data, encoding: String.Encoding.utf8.rawValue)!)
 //                    expectation2.fulfill()
 //                }
 //            })
+            print(NSString(data: newData!, encoding: String.Encoding.utf8.rawValue)!)
             expectation2.fulfill()
             
         }
